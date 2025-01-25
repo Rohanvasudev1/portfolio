bfa18b1ef597886dbb7eb47eb7cc87f0836a7437
rohanvasudev1 <rohan.vasudev@icloud.com>
2025-01-24 16:27:32 -0800

    steps 2

--- global.js
+++ global.js
@@ -3,3 +3,14 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+
+const navLinks = $$("nav a");
+
+
+const currentLink = navLinks.find(
+  (link) => link.host === location.host && link.pathname === location.pathname
+);
+
+
+currentLink?.classList.add("current");
