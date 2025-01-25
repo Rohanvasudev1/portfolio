1568de7e5cce5d381b198eaed95ded41d3747c6d
rohanvasudev1 <rohan.vasudev@icloud.com>
2025-01-24 16:53:54 -0800

    nav fixing

--- global.js
+++ global.js
@@ -1,16 +1,48 @@
-console.log('IT’S ALIVE!');
+console.log("IT’S ALIVE!");
+
 
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
 
+const pages = [
+  { url: "index.html", title: "Home" },
+  { url: "projects/index.html", title: "Projects" },
+  { url: "resume.html", title: "Resume" },
+  { url: "contact/index.html", title: "Contact" },
+  { url: "https://github.com/rohanvasudev1", title: "GitHub" },
+];
+
+
+const ARE_WE_HOME = document.documentElement.classList.contains("home");
+
+
+const nav = document.createElement("nav");
+document.body.prepend(nav);
+
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+
+  url = !ARE_WE_HOME && !url.startsWith("http") ? "../" + url : url;
+
+  let a = document.createElement("a");
+  a.href = url;
+  a.textContent = title;
+
+  a.classList.toggle(
+    "current",
+    a.host === location.host && a.pathname === location.pathname
+  );
+
+  if (a.host !== location.host) {
+    a.target = "_blank";
+  }
+
+  nav.append(a);
+}
+
 const navLinks = $$("nav a");
-
-
-const currentLink = navLinks.find(
-  (link) => link.host === location.host && link.pathname === location.pathname
-);
-
-
-currentLink?.classList.add("current");
+console.log("Navigation links added:", navLinks);
