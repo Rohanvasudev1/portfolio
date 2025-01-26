7e3422651c98d55f2e51f829f89e391c18786c5d
rohanvasudev1 <rohan.vasudev@icloud.com>
2025-01-25 20:16:23 -0800

    dark mode

--- global.js
+++ global.js
@@ -54,3 +54,34 @@ for (let p of pages) {
 
   nav.append(a);
 }
+
+document.body.insertAdjacentHTML(
+  'afterbegin',
+  `
+	<label class="color-scheme">
+		Theme:
+		<select id="theme-switcher">
+			<option value="auto">Automatic</option>
+			<option value="light">Light</option>
+			<option value="dark">Dark</option>
+		</select>
+	</label>`
+);
+
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
+const themeSelect = document.getElementById("theme-switcher");
+themeSelect.value = "auto"; 
+
+themeSelect.addEventListener("change", () => {
+  const value = themeSelect.value;
+  const root = document.documentElement;
+
+  if (value === "auto") {
+    root.style.colorScheme = "light dark"; 
+  } else {
+    root.style.colorScheme = value;
+  }
+});
+
+document.documentElement.style.colorScheme = prefersDarkScheme ? "dark" : "light";
+
