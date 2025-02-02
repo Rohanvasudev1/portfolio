6a8e263f585cf801bc8cd6ecb5b533435ac2021f
rohanvasudev1 <rohan.vasudev@icloud.com>
2025-02-01 23:33:47 -0800

    trying to push again

---
+++ README.md
@@ -0,0 +1,5 @@
+This is the code for my personal website displaying my projects and my resume as well as a contact form
+Still updating and adding new details 
+
+Here's the link to the website: 
+https://rohanvasudev1.github.io/portfolio/index.html
--- index.js
+++ index.js
@@ -4,32 +4,32 @@ const latestProjects = projects.slice(0, 3);
 const projectsContainer = document.querySelector('.projects');
 renderProjects(latestProjects, projectsContainer, 'h2');
 
-import { fetchGitHubData } from "./global.js";
+// import { fetchGitHubData } from "./global.js";
 
-async function loadGitHubProfile() {
-    const username = "rohanvasudev1"; 
-    const profileStats = document.querySelector("#profile-stats");
+// async function loadGitHubProfile() {
+//     const username = "rohanvasudev1"; 
+//     const profileStats = document.querySelector("#profile-stats");
 
-    if (!profileStats) return; // 
+//     if (!profileStats) return; // 
 
-    try {
-        const githubData = await fetchGitHubData(username);
+//     try {
+//         const githubData = await fetchGitHubData(username);
 
-        profileStats.innerHTML = `
-            <dl>
-                <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
-                <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
-                <dt>Followers:</dt><dd>${githubData.followers}</dd>
-                <dt>Following:</dt><dd>${githubData.following}</dd>
-            </dl>
-        `;
+//         profileStats.innerHTML = `
+//             <dl>
+//                 <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
+//                 <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
+//                 <dt>Followers:</dt><dd>${githubData.followers}</dd>
+//                 <dt>Following:</dt><dd>${githubData.following}</dd>
+//             </dl>
+//         `;
 
-    } catch (error) {
-        console.error("Error fetching GitHub profile data:", error);
-        profileStats.innerHTML = "<p>Failed to load GitHub profile.</p>";
-    }
-}
+//     } catch (error) {
+//         console.error("Error fetching GitHub profile data:", error);
+//         profileStats.innerHTML = "<p>Failed to load GitHub profile.</p>";
+//     }
+// }
 
-document.addEventListener("DOMContentLoaded", () => {
-    loadGitHubProfile();
-  });
+// document.addEventListener("DOMContentLoaded", () => {
+//     loadGitHubProfile();
+//   });
